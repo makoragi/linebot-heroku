13cc795291df0b070bd463da1467d8621769b8cd
makoragi <makoragi@gmail.com>
2016-04-13 22:38:49 +0900

    make throw requesting

--- web/index.php
+++ web/index.php
@@ -1,4 +1,12 @@
 <?php
-require('../vendor/autoload.php');
-echo "Hello ", getenv('VPS_URL'), "\n";
+$json_string = file_get_contents('php://input');
+
+$curl = curl_init(getenv('VPS_URL'));
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($curl, CURLOPT_POSTFIELDS, $json_string);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charser=UTF-8'));
+curl_exec($curl);
+curl_close($curl);
+?>
 
